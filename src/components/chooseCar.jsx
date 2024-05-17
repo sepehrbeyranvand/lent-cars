a7a6ae3320de715afe4a20e0762f34294402775b
sepehrbeyranvand <sepehr7797@gmail.com>
2024-05-18 00:01:00 +0330

    add json file

---
+++ src/components/chooseCar.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useState } from "react"
+
+
+export default function ChooseCar(){
+    const [url,setUrl] = useState('http://localhost:3000/')
+    const [cars,setCars] = useState([])
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
