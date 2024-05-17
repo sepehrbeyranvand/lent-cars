6fbc130734c576b8a491af59512e21fb70318496
sepehrbeyranvand <sepehr7797@gmail.com>
2024-05-17 19:43:38 +0330

    description about our services

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import firstCar from '../assets/firstCar.jpg'
 import GetCar from '@/components/getCar';
+import EasyGet from '@/components/easyGet';
 // import secondCar from '../assets/secondCar.png'
 // import thirdCar from '../assets/thirdCar.png'
 export default function Home() {
@@ -13,16 +14,17 @@ export default function Home() {
             <h1 className='text-6xl leading-[4rem] font-extrabold w-[70%]'>Save <span className='text-teal-600 font-extrabold'>BIG</span> with our car rental</h1>
             <p className=' text-neutral-400 font-light w-[70%]'>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
             <div className='flex gap-x-4 mt-6'>
-              <button className='text-white bg-teal-500 p-[.25rem] rounded-sm w-32 shadow-lg shadow-teal-200 hover:bg-teal-600 transition .4s ease-in-out duration-200'>Book One</button>
-              <button className='text-white bg-neutral-900 p-[.25rem] rounded-sm w-32 hover:bg-white hover:text-black transition .5s ease-in-out duration-300 border-2 font-semibold border-black outline-none'>Learn More <i className='fa fa-angle-right ms-[.378rem]'></i></button>
+              <button className='text-white bg-teal-500 p-[.45rem] rounded-sm w-36 shadow-lg shadow-teal-200 hover:bg-teal-600 transition .4s ease-in-out duration-200'>Book One</button>
+              <button className='text-white bg-neutral-900 p-[.45rem] rounded-sm w-36 hover:bg-white hover:text-black transition .5s ease-in-out duration-300 border-2 font-semibold border-black outline-none'>Learn More <i className='fa fa-angle-right ms-[.378rem]'></i></button>
             </div>
           </div>
           <div className='basis-1/2'>
-            <Image className=' mix-blend-multiply' width={650} src={firstCar}/>
+            <Image className=' mix-blend-multiply' width={680} src={firstCar}/>
           </div>
         </section>
         <GetCar></GetCar>
         </div>
+        <EasyGet></EasyGet>
     </div>
   );
 }
---
+++ src/components/easyGet.jsx
@@ -0,0 +1,40 @@
+import serviceAgent from "../assets/service-agent.png";
+import accept from "../assets/accept.png";
+import trip from "../assets/trip.png";
+import Image from "next/image";
+
+export default function EasyGet() {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-extrabold">
+        Quick & easy car rental
+      </h1>
+      <div className="flex flex-wrap justify-center items-center p-4 mt-14">
+        <div className="flex flex-col leading-9  w-[30%] justify-center items-center">
+          <Image src={accept} />
+          <h1 className="text-2xl font-bold">Select Car</h1>
+          <p className="text-center text-neutral-600">
+            We offers a big range of vehicles for all your driving needs. We
+            have the perfect car to meet your needs
+          </p>
+        </div>
+        <div className="flex flex-col leading-9  w-[30%] justify-center items-center">
+          <Image src={serviceAgent} />
+          <h1 className="text-2xl font-bold">Contact Operator</h1>
+          <p className="text-center text-neutral-600">
+            Our knowledgeable and friendly operators are always ready to help
+            with any questions or concerns
+          </p>
+        </div>
+        <div className="flex flex-col leading-9  w-[30%] justify-center items-center">
+          <Image src={trip} />
+          <h1 className="text-2xl font-bold">Let's Drive</h1>
+          <p className="text-center text-neutral-600">
+            Whether you're hitting the open road, we've got you covered with our
+            wide range of cars
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
