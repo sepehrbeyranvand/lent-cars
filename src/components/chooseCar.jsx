d5c37c0c6096e1bd9ead2b40153aa4eb6ef236c3
sepehrbeyranvand <sepehr7797@gmail.com>
2024-05-18 21:46:40 +0330

    solve the image bugs

--- src/components/chooseCar.jsx
+++ src/components/chooseCar.jsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
+// import firstID from '../assets/firstID.jpg'
+// import secondID from '../assets/secondID.jpg'
+// import thirdID from '../assets/thirdID.jpg'
+// import fourthID from '../assets/fourthID.jpg'
 import Image from "next/image";
 export default function ChooseCar() {
-  const [url, setUrl] = useState("http://localhost:3000/trips/1");
+  const [url, setUrl] = useState("http://localhost:3000/trips?id=1");
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
@@ -52,11 +55,40 @@ export default function ChooseCar() {
           </div>
         </div>
         <div className="basis-1/3">
-          {cars.map((e, index) => (
-            <Image width={600} height={600} src={e.src}/> 
-          ))}
+          {cars.length > 0 ? (
+            cars.map((e, index) => (
+              <div key={index}>
+                <Image width={600} height={600} src={e.src} />
+              </div>
+            ))
+          ) : (
+            <div>Loading...</div>
+          )}
         </div>
-        <div className="basis-1/3"></div>
+        <div className="basis-1/3">
+            {
+              cars.map((e)=>(
+                <table key={e.id} className=" ">
+                  <tr>
+                    <th>Model</th>
+                    <th>{e.name}</th>
+                  </tr>
+                  <tr>
+                    <td>Doors</td>
+                    <td>{e.doors}</td>
+                  </tr>
+                  <tr>
+                    <td>Year</td>
+                    <td>{e.year}</td>
+                  </tr>
+                  <tr>
+                    <td>Price</td>
+                    <td>{e.price}</td>
+                  </tr>
+                </table>
+              ))
+            }
+        </div>
       </div>
     </div>
   );
